aa0c02a58cfbbd863e138ca0e2ced650e2e11c49
MitchellMudd <mitchellmudd@outlook.com>
2021-06-21 13:01:03 -0400

    Fixed sign in button, started working on the book cards

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-function Book(title, author, numPages, hasItBeenRead, backgroundColor) {
-    this.title = title;
-    this.author = author;
-    this.numPages = numPages;
-    this.hasItBeenRead = hasItBeenRead;
-    this.backgroundColor = backgroundColor;
-  }
 
-let book1 = new Book("title", "person", 100, "yes", "green");
+
+function Book(title, author, numPages, language, hasItBeenRead, backgroundColor) {
+  this.title = title;
+  this.author = author;
+  this.numPages = numPages;
+  this.language = language;
+  this.hasItBeenRead = hasItBeenRead;
+  this.backgroundColor = backgroundColor;
+}
+
+let book1 = new Book("title", "person", "English", 100, "yes", "green");
 console.log(book1)
+
+function addBook() {
+  const main = document.getElementsByTagName("Main");
+  bookCard = document.createElement("div");
+  bookCard.classList.add("book")
+  main.appendChild(bookCard);
+}
