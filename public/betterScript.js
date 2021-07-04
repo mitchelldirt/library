4d966af54135c01a66912ad0b7d06f3a6ca4edca
Mitchell <mitchellmudd@outlook.com>
2021-07-04 14:25:42 -0400

    Made google sign in functional.

--- public/betterScript.js
+++ public/betterScript.js
@@ -15,6 +15,36 @@ let bookCards = document.getElementsByClassName("book");
 let stars = document.getElementsByClassName("star");
 let toggles = document.getElementsByClassName("toggle");
 
+//firebase Authentication
+const auth = firebase.auth()
+const whenSignedIn = document.getElementById("whenSignedIn");
+const whenSignedOut = document.getElementById("whenSignedOut");
+
+const signInBtn = document.getElementById("signInBtn");
+const signOutBtn = document.getElementById("signOutBtn");
+
+const userDetails = document.getElementById("userDetails");
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // signed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3>`
+    } else {
+        // not signed in
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
+    }
+});
+
 // Below is the code for the modal. This is used to grab input from the user for the book objects.
 const modal = document.getElementById("myModal");
 const btn = document.getElementById("addBookButton");
